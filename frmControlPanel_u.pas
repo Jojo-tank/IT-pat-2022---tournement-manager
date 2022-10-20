0bb5e332f1839047efc40ad174c7501bc4139212
Jojo-tank <115725628+Jojo-tank@users.noreply.github.com>
2022-10-20 07:21:15 +0200

    Add files via upload

--- frmControlPanel_u.pas
+++ frmControlPanel_u.pas
@@ -4,10 +4,23 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
 
 type
   TfrmControlPanel = class(TForm)
+    lblCurrentRound: TLabel;
+    pnlInput: TPanel;
+    cbxTask: TCheckBox;
+    lblInput: TLabel;
+    lblTime: TLabel;
+    edtTime: TEdit;
+    btnInput: TButton;
+    btnFinish: TButton;
+    cmxPlayer: TComboBox;
+    btnNext: TButton;
+    btnSTOP: TButton;
+    lblWarning: TLabel;
+
   private
     { Private declarations }
   public
@@ -20,5 +33,7 @@ var
 implementation
 
 {$R *.dfm}
+
+
 
 end.
